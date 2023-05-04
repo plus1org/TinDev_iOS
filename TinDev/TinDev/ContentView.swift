e1e0481237cd84194f5a44060419ec0a37cf3b27
stacy177 <75451219+stacy177@users.noreply.github.com>
2023-05-04 11:55:31 +0300

    initial commit

---
+++ TinDev/TinDev/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TinDev
+//
+//  Created by Анастасия on 04.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinDev/TinDev/TinDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinDevApp.swift
+//  TinDev
+//
+//  Created by Анастасия on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TinDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
