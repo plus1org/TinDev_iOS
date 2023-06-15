0d63f857e142d80bc94fa86b34c4518adb97287f
stacy177 <75451219+stacy177@users.noreply.github.com>
2023-06-15 16:22:10 +0300

    fix TextField

--- TinDev/TinDev/PresentationLayer/Components/TextFieldStandart.swift
+++ TinDev/TinDev/PresentationLayer/Components/TextFieldStandart.swift
@@ -6,43 +6,30 @@ import SwiftUI
 
 struct TextFieldStandart: View {
     let headerText: String
-    var state: TextFieldState = .empty
     
-    @State var text: String
+    @State private var isEditing = false
+    @State var text: String = ""
+    
     let placeholder: String
     
     let textForegroundColor: Color
     
     var body: some View {
         VStack(alignment: .leading) {
-            switch state {
-            case .empty:
                 Text(headerText)
                     .foregroundColor(textForegroundColor)
                     .font(Fonts.regular15)
-                
-                TextField(placeholder, text: $text)
-                    .frame(height: 50)
-                    .font(Fonts.regular14)
-                    .padding(.leading)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Pallete.customGray, lineWidth: 1)
-                    )
-            case .withText:
-                Text(headerText)
-                    .foregroundColor(textForegroundColor)
-                    .font(Fonts.regular15)
-                
-                TextField(placeholder, text: $text)
-                    .frame(height: 50)
-                    .font(Fonts.regular14)
-                    .padding(.leading)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Pallete.customBlack, lineWidth: 1.5)
-                    )
-            }
+            TextField(placeholder, text: $text, onEditingChanged: { editing in
+                self.isEditing = editing
+            }, onCommit: {
+            })
+            .frame(height: 50)
+            .font(Fonts.regular14)
+            .padding(.leading)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(isEditing ? Pallete.customBlack : Pallete.customGray, lineWidth: 2)
+            )
         }
     }
 }
@@ -52,10 +39,5 @@ struct TextFieldStandart_Previews: PreviewProvider {
         TextFieldStandart(headerText: "Label", text: "", placeholder: "ввдедите имя", textForegroundColor: .black)
         
     }
-}
-
-enum TextFieldState {
-    case empty
-    case withText
 }
 
