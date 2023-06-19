590bea1b76153a772d378a420ec429d4aa84b497
mttm <macbookpro@MacBooks-MacBook-Pro.local>
2023-06-19 13:46:31 +0300

    ChatScreen added

--- TinDev/TinDev/TinDevApp.swift
+++ TinDev/TinDev/TinDevApp.swift
@@ -6,9 +6,21 @@ import SwiftUI
 
 @main
 struct TinDevApp: App {
+    
+    static var chat = Chat(
+        personImage: "person",
+        personName: "Игорь Немцов",
+        messageDate: "15 июн",
+        messages: [
+            Message(text: "Привет, как дела?", isUserMessage: false),
+            Message(text: "Привет, все отлично!", isUserMessage: true),
+            Message(text: "Договорились, тогда я буду рассчитывать на тебя", isUserMessage: false)
+        ]
+    )
+    
     var body: some Scene {
         WindowGroup {
-            RegistrationView()
+            ChatView(chat: TinDevApp.chat)
         }
     }
 }
