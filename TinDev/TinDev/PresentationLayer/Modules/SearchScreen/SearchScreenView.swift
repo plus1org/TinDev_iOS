98999ebb2504b53bfabdf3af4218ead696772e00
M.Arkatov <mxm.arkatov@gmail.com>
2023-06-26 15:07:27 +0300

    #2 Экран Авторизации moved Strings to enum and created folder for SearchScreen

---
+++ TinDev/TinDev/PresentationLayer/Modules/SearchScreen/SearchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchScreenView.swift
+//  TinDev
+//
+//  Created by M on 26.06.2023.
+//
+
+import SwiftUI
+
+struct SearchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchScreenView()
+    }
+}
---
+++ TinDev/TinDev/PresentationLayer/Modules/SearchScreen/SearchScreenViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchScreenViewModel.swift
+//  TinDev
+//
+//  Created by M on 26.06.2023.
+//
+
+import Foundation
